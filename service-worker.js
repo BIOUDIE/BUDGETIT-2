48369b738bd6757e069636b2ea8ec9fa08c7a041
BIOUDIE <41976717+BIOUDIE@users.noreply.github.com>
2025-09-26 16:38:53 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,28 +2,22 @@ const CACHE_NAME = "elektro-budgetit-v2";
 const ASSETS = [
   "/",
   "/index.html",
+  "/style.css",
+  "/app.js",
   "/manifest.json",
   "/icon-192.png",
-  "/icon-512.png",
-  "/style.css",
-  "/app.js"
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
@@ -31,11 +25,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return (
-        response ||
-        fetch(event.request).catch(() => caches.match("/index.html"))
-      );
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
